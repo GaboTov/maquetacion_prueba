e76ad33cf74f824157135ecb6f01d31813295eea
Gabo Tovar <gabotovar7@gmail.com>
2023-07-14 13:47:16 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('funciona');
+const collapse = (idContent, idBtn) => {
+    const contenido = document.getElementById(idContent);
+    const btn = document.getElementById(idBtn);
+    contenido.classList.toggle('cerrado');
+    if (contenido.classList.contains('cerrado')) {
+        btn.textContent = '+';
+    }
+    else {
+        btn.textContent = '-';
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-console.log('funciona')
 
 
 
+const collapse = (idContent:string, idBtn:string)=> {
+    const contenido = document.getElementById(idContent)
+    const btn = document.getElementById(idBtn)
+    contenido.classList.toggle('cerrado')
+    if (contenido.classList.contains('cerrado')){
+        btn.textContent = '+'
+    }else{
+        btn.textContent = '-'
+    }
+}
